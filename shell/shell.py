15cd4662f90ba033a6dc3932b89605a4abb67727
Your Name <you@example.com>
2020-09-09 14:00:03 -0600

    added user input first try

---
+++ shell/shell.py
@@ -0,0 +1,14 @@
+while True:
+        if 'PS1' in os.environ:
+            os.write(1, (os.environ['PS1']).encode())
+        else:
+            os.write(1, ('$ ').encode())
+            try:
+                userInput = input()
+            except EOFError:
+                sys.exit(1)
+
+        if userInput == "": # Empty input, will prompt again
+            continue
+        if 'exit' in userInput: # Terminates shell
+            break
