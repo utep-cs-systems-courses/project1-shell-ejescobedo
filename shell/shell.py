fa1f54588182d0df5401dca561f455f781eb8d87
Your Name <you@example.com>
2020-09-13 01:14:59 -0600

    Problem with the pipe

--- shell/shell.py
+++ shell/shell.py
@@ -55,10 +55,55 @@ while True:
                 os.write(2, ("fork failed, returning %d\n" % rc).encode())
                 sys.exit(1)
         elif rc == 0:                   # child
+
                 os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %(os.getpid(), pid)).encode())
                 args = userInput.split()
                 #os.write(2,("%s\n"%userInput).encode())
 
+                if "|" in userInput: # Piping command
+                        pipe = userInput.split("|")
+                        pipeCommand1= pipe[0].split()
+                        pipeCommand2 = pipe[1].split()
+
+                        pr, pw = os.pipe()  # file descriptors pr, pw for reading and writing
+                        for f in (pr, pw):
+                                os.set_inheritable(f, True)
+                        pipeFork = os.fork()
+                        if pipeFork < 0:  # fork failed
+                                os.write(2, ('Fork failed').encode())
+                                sys.exit(1)
+
+                        if pipeFork == 0: # child - will write to pipe
+                                os.close(1) # redirect child's stdout
+                                os.dup(pw)
+                                os.set_inheritable(1, True)
+                        
+                                for fd in (pr, pw):
+                                        os.close(fd)
+                                for dir in re.split(':', os.environ['PATH']): # try 
+                                        program = "%s/%s" % (dir, pipeCommand1[0])
+                                        try:
+                                                os.execve(program, pipeCommand1, os.environ) # try to exec program
+                                        except FileNotFoundError:                  # ...expected
+                                                pass                            # ...fail quietly
+                                os.write(2, ("Could not exec %s\n" % pipeCommand[0]).encode())
+                                sys.exit(1)    
+                        else: # parent (forked ok)
+                                os.close(0)
+                                os.dup(pr)
+                                os.set_inheritable(0, True)
+                                for fd in (pw, pr):
+                                        os.close(fd)
+                                for dir in re.split(':', os.environ['PATH']): # try 
+                                        program = "%s/%s" % (dir, pipeCommand2[0].split())
+                                        try:
+                                                os.execve(program, pipeCommand2, os.environ) # try to exec program
+                                        except FileNotFoundError:                  # ...expected
+                                                pass                            # ...fail quietly
+                                os.write(2, ("Could not exec %s\n" % pipeCommand2[0].split()).encode())
+                                sys.exit(1)
+        
+                                
                 if '>' in userInput:
                         redirect('>', userInput)
                 elif '<' in userInput:
